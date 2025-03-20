31383e14b720283f9ee9b85cbf84f7dc8009e947
alexey <1@tcrossa.ru>
2025-03-20 13:46:01 +0700

    #todo проверить результат вывода код выводит первую !

--- docs/examples/amazon_product_extraction_using_hooks.py
+++ docs/examples/amazon_product_extraction_using_hooks.py
@@ -4,6 +4,8 @@ from crawl4ai.async_configs import BrowserConfig, CrawlerRunConfig
 import json
 from playwright.async_api import Page, BrowserContext
 
+#todo предусмотреть вывод всех стро а не только 10
+
 async def extract_drom_products():
     # Конфигурация браузера
     browser_config = BrowserConfig(
